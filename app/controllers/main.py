8b03b553c7a5e70067a1ef1f28493d7e25c7b413
Haider Ali Khan <haiderk0987@gmail.com>
2023-03-31 07:45:04 +0500

    chore: single quotes instead of double

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -8,7 +8,7 @@ bp = Blueprint('main', __name__)
 def timeline():
 	db = get_db()
 
-	events = db.execute("SELECT * FROM tabEvent").fetchall()
+	events = db.execute('SELECT * FROM tabEvent').fetchall()
 
 	events_data = {}
 
